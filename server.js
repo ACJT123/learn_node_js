011902603b4aaf61d2b118f335ae89e12767187d
acjta <chengjt-wp20@student.tarc.edu.my>
2024-03-27 22:46:13 +0800

    send html  page to browser

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+
+  });
 });
 
 server.listen(5000, "localhost", () => {
-    console.log('listening for request');
+  console.log("listening for request");
 });
