8d266dc3c3fa5654b767b715694d1f84c0b61bd2
acjta <chengjt-wp20@student.tarc.edu.my>
2024-03-28 20:29:51 +0800

    use ejs to display dynamic content

--- app.js
+++ app.js
@@ -3,22 +3,60 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(5000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Yoshi finds eggs",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      content: "Lorem ipsum dolor sit amet consectetur",
+    },
+    
+  ];
+  res.render("index", { title: "home", blogs });
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+// // redirects
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "page not found" });
 });
