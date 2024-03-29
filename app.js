c73b16a2c1f9f8b2dd57772d7f9dfdc66a910421
alex <alexcheng@vvinners.com>
2024-03-29 12:30:31 +0800

    CR to mongodb  using mongoose

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const Blog = require("./models/blog");
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://acjt:4g0do5HbfanQxnlN@cluster0.be0fbze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("connected to db");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // express app
 const app = express();
@@ -7,47 +23,62 @@ const app = express();
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(5000);
-
 // middleware & static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add_blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    content: "this is my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all_blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/get_blog", (req, res) => {
+  Blog.findById('66064204c6337849e3bb0854')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Yoshi finds eggs",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      content: "Lorem ipsum dolor sit amet consectetur",
-    },
-    
-  ];
-  res.render("index", { title: "home", blogs });
+  res.redirect('/blogs');
 });
+
+// blog routes
+app.get('/blogs', (req, res)=>{
+  Blog.find().sort('create_at')
+    .then((result)=>{
+      res.render('index', { title: 'All blogs', blogs: result})
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
 });
